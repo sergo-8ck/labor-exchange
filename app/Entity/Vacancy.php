e48e672f5892b756ea13727a5190dcd76662ccf8
sergo-8ck <sergo-8ck@yandex.ru>
2018-08-30 18:24:08 +0300

    Model Vacancy and methods

--- app/Entity/Vacancy.php
+++ app/Entity/Vacancy.php
@@ -2,7 +2,10 @@
 
 namespace App\Entity;
 
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * @property int $id
@@ -10,11 +13,50 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $title
  * @property string $content
  * @property string $status
+ *
+ * @property User $user
  */
 class Vacancy extends Model
 {
   public const STATUS_MODERATION = 'moderation';
   public const STATUS_ACTIVE = 'active';
 
-  protected $table = 'advert_adverts';
+  protected $table = 'vacancy';
+
+  protected $casts = [
+    'expires_at' => 'datetime',
+  ];
+
+  public function sendToModeration(): void
+  {
+    $this->update([
+      'status' => self::STATUS_MODERATION,
+    ]);
+  }
+
+  public function moderate(Carbon $date): void
+  {
+    if ($this->status !== self::STATUS_MODERATION) {
+      throw new \DomainException('Vacancy is not sent to moderation.');
+    }
+    $this->update([
+      'published_at' => $date,
+      'status' => self::STATUS_ACTIVE,
+    ]);
+  }
+
+  public function isActive(): bool
+  {
+    return $this->status === self::STATUS_ACTIVE;
+  }
+
+  public function user()
+  {
+    return $this->belongsTo(User::class, 'user_id', 'id');
+  }
+
+  public function scopeForUser(Builder $query, User $user)
+  {
+    return $query->where('user_id', $user->id);
+  }
 }
